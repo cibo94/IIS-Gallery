2000b0f3b3b785374045d2f10b7f561c3828cc3f
Miroslav Cibulka <xcibul10@stud.fit.vutbr.cz>
2015-11-22 16:44:46 +0100

    Bootstrap Material theme added

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,45 +1,21 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
     <head>
         <title>Laravel</title>
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
+        {!! Html::style('css/bootstrap.min.css') !!}
 
-        <style>
-            html, body {
-                height: 100%;
-            }
-
-            body {
-                margin: 0;
-                padding: 0;
-                width: 100%;
-                display: table;
-                font-weight: 100;
-                font-family: 'Lato';
-            }
-
-            .container {
-                text-align: center;
-                display: table-cell;
-                vertical-align: middle;
-            }
-
-            .content {
-                text-align: center;
-                display: inline-block;
-            }
-
-            .title {
-                font-size: 96px;
-            }
-        </style>
     </head>
     <body>
         <div class="container">
-            <div class="content">
-                <div class="title">Laravel 5</div>
+            <div class="row">
+                <h2>This template is using Bootstrap from a CDN!</h2>
             </div>
         </div>
+
+        <script src="https://code.jquery.com/jquery-1.10.2.min.js"></script>
+        {!! HTML::script('js/bootstrap.min.js') !!}
     </body>
 </html>
