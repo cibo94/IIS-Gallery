59299a3c8f8df52d7daf3f88687afdf37e33f1b1
Lukas Slouka <xslouk02@stud.fit.vutbr.cz>
2015-11-24 17:18:47 +0100

    Home page remade + controller

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -8,12 +8,37 @@
 
 namespace App\Http\Controllers;
 
-
+use DB;
+use Schema;
 
 class HomeController extends Controller
 {
     public function get()
     {
-        return view("home");
+
+        $date = 
+        // Select vsetky vystavovane diela
+        $select = 'SELECT Z.odCas od, Z.doCas do, D.nazov Nazov, 
+                          A.meno, D.typ Typ, E.Nazov Nazov_Expozicie
+                    FROM Autor A 
+                    INNER JOIN Dielo D ON A.IDAUTORA = D.IDAUTORA
+                    INNER JOIN Zahrnuje Z ON Z.DIELO = D.IDDIELA 
+                                             AND Z.doCas >= CURDATE()
+                                             AND Z.odCas <= CURDATE()
+                    INNER JOIN Expozicia E WHERE E.IDexpozicie IN 
+                        (SELECT Za.expozicia
+                         FROM ExpozicneMiesto EX, Zahrnuje Za
+                         WHERE EX.IDmiesta = Za.expozicneMiesto
+                         AND Za.dielo = D.IDdiela)
+                    ORDER BY do';
+                        
+        $table = DB::select($select);
+        $header = ['Vystavované od', 'Vystavované do', 'Názov diela', 
+                   'Autor', 'Typ diela', 'Expozícia'];
+
+        return view("home")->with([
+            'header' => $header,
+            'table' => $table
+        ]);
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,35 +3,15 @@
 @section('content')
 
     <div class="row">
-        <h1>Welcome to my website</h1>
-        <p>We are creating something beautiful today.</p>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-            quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-            consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-            cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-            proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        <center><h1>Galéria</h1></center>
+        <center><p>Najlepšia neexistujúca galéria na svete</p></center>
+        <center>
+        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/ec/Mona_Lisa%2C_by_Leonardo_da_Vinci%2C_from_C2RMF_retouched.jpg/800px-Mona_Lisa%2C_by_Leonardo_da_Vinci%2C_from_C2RMF_retouched.jpg" 
+             style="width:212px;height:308px;">
+        <h2>Práve vystavujeme</h2></center>
 
     </div>
 
-    @include('table', ['table' => DB::select('select * from Autor'), 'header' => Schema::getColumnListing('Autor')])
-
-    <div class="form-group label-static">
-        <label for="i2" class="control-label">label-static</label>
-        <input type="email" class="form-control" id="i2" placeholder="placeholder attribute">
-        <p class="help-block">This is a hint as a <code>p.help-block.hint</code></p>
-    </div>
-
-    <div class="form-group label-floating">
-        <label for="i5" class="control-label">label-floating</label>
-        <input type="email" class="form-control" id="i5">
-        <span class="help-block">This is a hint as a <code>span.help-block.hint</code></span>
-    </div>
-
-    <div class="form-group label-placeholder">
-        <label for="i5p" class="control-label">label-placeholder</label>
-        <input type="email" class="form-control" id="i5p">
-        <span class="help-block">This is a hint as a <code>span.help-block.hint</code></span>
-    </div>
+    @include('table', ['table' => $table, 'header' => $header])
 
 @stop
