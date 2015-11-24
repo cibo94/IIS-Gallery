cd89c938adff6597dc72e7377658afce8b0109c2
Miroslav Cibulka <xcibul10@stud.fit.vutbr.cz>
2015-11-24 14:29:09 +0100

    redirect mistake in nav bar

--- resources/views/baselayout.blade.php
+++ resources/views/baselayout.blade.php
@@ -29,16 +29,16 @@
         </div>
         <div id="navbar" class="collapse navbar-collapse">
             <ul class="nav navbar-nav">
-                <li><a href="home">Home</a></li>
-                <li><a href="#about">About</a></li>
+                <li><a href="/home">Home</a></li>
+                <li><a href="/#about">About</a></li>
             </ul>
             <ul class="nav navbar-nav navbar-right">
                 @if (Auth::check())
-                    <li><a href="#">Logged in as '{!! Auth::user()->name !!}'</a></li>
-                    <li><a href="auth/logout">Logout</a></li>
+                    <li><a href="/#">Logged in as '{!! Auth::user()->name !!}'</a></li>
+                    <li><a href="/auth/logout">Logout</a></li>
                 @else
-                    <li><a href="auth/login">Login</a></li>
-                    <li><a href="auth/register">Sign in</a></li>
+                    <li><a href="/auth/login">Login</a></li>
+                    <li><a href="/auth/register">Sign in</a></li>
                 @endif
             </ul>
         </div><!--/.nav-collapse -->
